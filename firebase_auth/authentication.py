bced6a2ae75f07627fef745229ea3db067c0d625
Felix Cornelius <mail@felixcornelius.de>
2017-08-11 20:10:10 +0200

    create class FirebaseAuthentication

--- firebase_auth/authentication.py
+++ firebase_auth/authentication.py
@@ -1,3 +1,6 @@
+from rest_framework import authentication
+from django.contrib.auth.models import User
+from rest_framework import exceptions
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
@@ -7,4 +10,14 @@ from django.conf import settings
 json = os.path.join(settings.KEYFILES_DIR, settings.FIREBASE_KEY)
 cred = credentials.Certificate(json)
 default_app = firebase_admin.initialize_app(cred)
+
+
+class FirebaseAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        id_token = request.META.get('HTTP_AUTHORIZATION')
+        decoded_token = None
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+        except Exception as e:
+            pass
 
