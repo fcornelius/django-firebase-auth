cbedef023480bca050b28568709bd595e5e951c4
Felix Cornelius <mail@felixcornelius.de>
2017-08-15 21:33:10 +0200

    set authentication_classes in FirebaseAuthMixin

--- firebase_auth/mixins.py
+++ firebase_auth/mixins.py
@@ -2,4 +2,5 @@ from .authentication import FirebaseAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class FirebaseAuthMixin():
-
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (FirebaseAuthentication,)
