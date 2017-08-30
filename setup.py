31f524cde95a47a3ef14a9b6da90b34423dbad43
Felix Cornelius <mail@felixcornelius.de>
2017-08-30 10:12:04 +0200

    add meta infos to setup.py

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-firebase-auth',
+    version='0.4',
+    packages=find_packages()
+)
